f404ebf36ea1798771dbfeacfd164f89ad5c1625
Syleront <guttaviva@gmail.com>
2019-04-07 16:10:44 +0300

    v0.3.3

--- script.user.js
+++ script.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name SAS
 // @description Smotretanime-Ad-Skiper
-// @version 0.3.2
+// @version 0.3.3
 // @author Syleront
 // @include /https?:\/\/smotretanime\.ru\/.+/embed/
 // @connect smotretanime.ru
@@ -20,34 +20,49 @@
   const overlay_text = "Тут реклама братан, ща уберем подожди";
   const TempData = {};
 
-  let listener = new EventListener();
-  listener.on("activation_key", (key) => {
-    console.log("[ad-skiper] ad-key received");
-    showWaitOverlay(false);
+  window.addEventListener("DOMContentLoaded", () => {
+    let skip_button = document.querySelector(".skip-button");
+    if (skip_button !== null) {
+      console.log("[ad-skiper] ad detected");
+      showWaitOverlay(false);
 
-    let sas_key = localStorage.getItem("sas-key");
-    if (sas_key) {
-      console.log("[ad-skiper] using key from cache");
-      key = sas_key;
-    } else {
-      console.log("[ad-skiper] cache is empty, using new key");
-      localStorage.setItem("sas-key", key);
+      let sas_key = localStorage.getItem("sas-key");
+      if (sas_key !== null) {
+        console.log("[ad-skiper] using key from cache");
+        getAndSetPromoCode(sas_key);
+      } else {
+        console.log("[ad-skiper] cache is empty, getting a new key");
+        initSasKeyGetter();
+      }
     }
-
-    document.addEventListener("DOMContentLoaded", () => {
-      getAndSetPromoCode(key);
-    });
   });
 
-  window.addEventListener("sasKey", (evt) => {
-      listener.emit("activation_key", encodeURIComponent(evt.detail));
-  });
-
-  unsafeWindow.addEventListener("load", () => {
+  window.addEventListener("load", () => {
     unsafeWindow.playerGlobal.concatenate.notActivatedAlert = () => {
       console.log("[ad-skiper] activation alert bypassed");
     };
   });
+
+  function initSasKeyGetter() {
+    window.addEventListener("sasKey", (evt) => {
+      let key = encodeURIComponent(evt.detail);
+      localStorage.setItem("sas-key", key);
+      getAndSetPromoCode(key);
+    });
+
+    request(document.location.href, {
+      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
+      "upgrade-insecure-requests": 1
+    }).then((body) => {
+      let matched = body.match(/data:text\/javascript;base64,(.+?)"/);
+      if (matched) {
+        let code = atob(matched[1]).replace(/(delete window\.activateCodeTmp;).*/, "$1 var event = new CustomEvent('sasKey', { 'detail': activateCode }); window.dispatchEvent(event); return;");
+        let script = document.createElement("script");
+        script.src = `data:text/javascript;base64,${btoa(code)}`;
+        document.body.appendChild(script);
+      }
+    });
+  }
 
   function getAndSetPromoCode(key) {
     let url = `https://smotretanime.ru/translations/embedActivation?code=${key}`;
@@ -115,41 +130,6 @@
         }
       })();
     }
-  }
-
-  function EventListener() {
-    let events = [];
-
-    this.emit = (name, data) => {
-      events.forEach((e) => {
-        if (e.name == name) {
-          e._cb(data);
-        }
-      });
-    };
-
-    this.on = (name, _cb) => {
-      events.push({ name, _cb });
-    };
-
-    new MutationObserver((mutations) => {
-      mutations.forEach((mutation) => {
-        mutation.addedNodes.forEach((node) => {
-          if (node.tagName === "SCRIPT" && node.id) {
-            let matched = node.src.match(/data:text\/javascript;base64,(.+?)$/i);
-            if (matched) {
-              let code = atob(matched[1]).replace(/(delete window\.activateCodeTmp;).*/, "$1 var event = new CustomEvent('sasKey', { 'detail': activateCode }); window.dispatchEvent(event); return;");
-              let script = document.createElement("script");
-              script.src = `data:text/javascript;base64,${btoa(code)}`;
-              document.body.appendChild(script);
-            }
-          }
-        });
-      });
-    }).observe(document, {
-      childList: true,
-      subtree: true
-    });
   }
 
   function request(url, headers) {
