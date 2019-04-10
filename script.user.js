bb234a693b180cac74bf408fc68cc0d0114675f2
Syleront <guttaviva@gmail.com>
2019-04-10 04:46:23 +0300

    v0.4.0

--- script.user.js
+++ script.user.js
@@ -1,11 +1,10 @@
 // ==UserScript==
 // @name SAS
 // @description Smotretanime-Ad-Skiper
-// @version 0.3.3
+// @version 0.4.0
 // @author Syleront
 // @include /https?:\/\/smotretanime\.ru\/.+/embed/
 // @connect smotretanime.ru
-// @grant GM_xmlhttpRequest
 // @run-at document-start
 // @copyright 2018, Syleront
 // @homepage https://github.com/syleront/smotretanime-ad-skiper
@@ -20,78 +19,111 @@
   const overlay_text = "Тут реклама братан, ща уберем подожди";
   const TempData = {};
 
+  if (!window.unsafeWindow) window.unsafeWindow = typeof unsafeWindow !== "undefined" ? unsafeWindow : window;
+  if (!window.exportFunction) window.exportFunction = typeof exportFunction !== "undefined" ? exportFunction : null;
+
+  if (!window.unsafeWindow) window.unsafeWindow = window;
+  if (!window.exportFunction) window.exportFunction = function (f) { return f; };
+
+  let listener = new createElementListener();
+  listener.on("created", (node) => {
+    if (node.tagName === "IFRAME") {
+      document.body.appendChild = (elem) => {
+        let result = Element.prototype.appendChild.bind(document.body, elem)();
+
+        if (elem.tagName === "IFRAME") {
+          elem.contentWindow.window.HTMLAudioElement.prototype.play = () => { };
+        }
+
+        return result;
+      };
+    }
+  });
+
+  let skip_button;
+
   window.addEventListener("DOMContentLoaded", () => {
-    let skip_button = document.querySelector(".skip-button");
+    skip_button = document.querySelector(".skip-button");
     if (skip_button !== null) {
       console.log("[ad-skiper] ad detected");
       showWaitOverlay(false);
+    }
+  });
 
+  window.addEventListener("load", () => {
+    window.unsafeWindow.playerGlobal.concatenate.notActivatedAlert = () => {
+      console.log("[ad-skiper] activation alert bypassed");
+    };
+
+    let yt_elems = document.querySelectorAll(".seed-player-container");
+    if (yt_elems.length) {
+      console.log("[ad-skiper] removing youtube ad");
+      yt_elems.forEach((e) => e.remove());
+    }
+
+    if (skip_button) {
       let sas_key = localStorage.getItem("sas-key");
       if (sas_key !== null) {
         console.log("[ad-skiper] using key from cache");
         getAndSetPromoCode(sas_key);
       } else {
         console.log("[ad-skiper] cache is empty, getting a new key");
-        initSasKeyGetter();
+        window.unsafeWindow.playerGlobal.concatenate.activate = window.exportFunction((code) => {
+          let key = encodeURIComponent(code);
+          localStorage.setItem("sas-key", key);
+          getAndSetPromoCode(key);
+        }, window.unsafeWindow);
+
+        document.querySelector(".vjs-big-play-button").click();
+        showWaitOverlay(true);
       }
     }
   });
 
-  window.addEventListener("load", () => {
-    unsafeWindow.playerGlobal.concatenate.notActivatedAlert = () => {
-      console.log("[ad-skiper] activation alert bypassed");
+  function createElementListener() {
+    let events = [];
+
+    this.emit = (name, data) => {
+      events.forEach((e) => {
+        if (e.name == name) {
+          e._cb(data);
+        }
+      });
     };
-  });
 
-  function initSasKeyGetter() {
-    window.addEventListener("sasKey", (evt) => {
-      let key = encodeURIComponent(evt.detail);
-      localStorage.setItem("sas-key", key);
-      getAndSetPromoCode(key);
-    });
+    this.on = (name, _cb) => {
+      events.push({ name, _cb });
+    };
 
-    request(document.location.href, {
-      "accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3",
-      "upgrade-insecure-requests": 1
-    }).then((body) => {
-      let matched = body.match(/data:text\/javascript;base64,(.+?)"/);
-      if (matched) {
-        let code = atob(matched[1]).replace(/(delete window\.activateCodeTmp;).*/, "$1 var event = new CustomEvent('sasKey', { 'detail': activateCode }); window.dispatchEvent(event); return;");
-        let script = document.createElement("script");
-        script.src = `data:text/javascript;base64,${btoa(code)}`;
-        document.body.appendChild(script);
-      }
-    });
+    let ctx = this;
+    let createElement = document.createElement;
+    document.createElement = function (...args) {
+      let elem = createElement.bind(document)(...args);
+      ctx.emit("created", elem);
+      return elem;
+    };
   }
 
   function getAndSetPromoCode(key) {
+    console.log("[ad-skiper] getting response...");
     let url = `https://smotretanime.ru/translations/embedActivation?code=${key}`;
-    (function get(url, is_reconnect) {
-      console.log("[ad-skiper] getting response...");
-      request(url, {
-        "Accept": "application/json, text/javascript, */*; q=0.01",
-        "x-requested-with": "XMLHttpRequest"
-      }).then((body) => {
-        let res = JSON.parse(body);
-        if (res === null || res.error && is_reconnect) {
-          console.log("[ad-skiper] ad-key is wrong, removing it and try again...");
-          localStorage.removeItem("sas-key");
+    request(url, {
+      "Accept": "application/json, text/javascript, */*; q=0.01",
+      "x-requested-with": "XMLHttpRequest"
+    }).then((body) => {
+      let res = JSON.parse(body);
+      if (res.error) {
+        console.log("[ad-skiper] ad-key is wrong, removing it and try again...");
+        localStorage.removeItem("sas-key");
+        window.location.reload();
+      } else {
+        console.log("[ad-skiper] set cookie");
+        setCookie("watchedPromoVideo", res.cookieValue);
+        setTimeout(() => {
           window.location.reload();
-        } else if (res.error) {
-          console.log(`[ad-skiper] error, retry after ${wait_timeout} sec...`);
-          showWaitOverlay(true);
-          setTimeout(() => {
-            get(url, true);
-          }, wait_timeout * 1000);
-        } else {
-          console.log("[ad-skiper] set cookie");
-          setCookie("watchedPromoVideo", res.cookieValue);
-          setTimeout(() => {
-            window.location.reload();
-          }, 500);
-        }
-      });
-    })(url);
+        }, 500);
+      }
+    });
   }
 
   function showWaitOverlay(is_main) {
@@ -103,8 +135,8 @@
     overlayDiv.style.width = "100%";
     overlayDiv.style.height = "100%";
     overlayDiv.style.background = "#000";
-    overlayDiv.style.zIndex = "1000";
-    overlayDiv.style.opacity = "0.8";
+    overlayDiv.style.zIndex = "10000";
+    overlayDiv.style.opacity = "0.9";
 
     let textDiv = document.createElement("div");
     textDiv.style.textAlign = "center";
